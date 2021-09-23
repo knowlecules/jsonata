d7567346548a876577e7bfd45d2826c896e8321b
Jason Rudland <knowlecules@gmail.com>
2021-09-23 18:17:08 -0400

    more necessary jsonata files

---
+++ jsonata-es5.js
@@ -0,0 +1,51 @@
+Usage: regenerator [options] <source directory> <output directory> [<module ID> [<module ID> ...]]
+
+Options:
+  -V, --version                            output the version number
+  -c, --config [file]                      JSON configuration file (no file or - means STDIN)
+  -w, --watch                              Continually rebuild
+  -x, --extension <js | coffee | ...>      File extension to assume when resolving module identifiers
+  --relativize                             Rewrite all module identifiers to be relative
+  --follow-requires                        Scan modules for required dependencies
+  --use-provides-module                    Respect @providesModules pragma in files
+  --cache-dir <directory>                  Alternate directory to use for disk cache
+  --no-cache-dir                           Disable the disk cache
+  --source-charset <utf8 | win1252 | ...>  Charset of source (default: utf8)
+  --output-charset <utf8 | win1252 | ...>  Charset of output (default: utf8)
+  -r, --include-runtime                    Prepend the runtime to the output.
+  --disable-async                          Disable transformation of async functions.
+  -h, --help                               output usage information
+Usage: regenerator [options] <source directory> <output directory> [<module ID> [<module ID> ...]]
+
+Options:
+  -V, --version                            output the version number
+  -c, --config [file]                      JSON configuration file (no file or - means STDIN)
+  -w, --watch                              Continually rebuild
+  -x, --extension <js | coffee | ...>      File extension to assume when resolving module identifiers
+  --relativize                             Rewrite all module identifiers to be relative
+  --follow-requires                        Scan modules for required dependencies
+  --use-provides-module                    Respect @providesModules pragma in files
+  --cache-dir <directory>                  Alternate directory to use for disk cache
+  --no-cache-dir                           Disable the disk cache
+  --source-charset <utf8 | win1252 | ...>  Charset of source (default: utf8)
+  --output-charset <utf8 | win1252 | ...>  Charset of output (default: utf8)
+  -r, --include-runtime                    Prepend the runtime to the output.
+  --disable-async                          Disable transformation of async functions.
+  -h, --help                               output usage information
+Usage: regenerator [options] <source directory> <output directory> [<module ID> [<module ID> ...]]
+
+Options:
+  -V, --version                            output the version number
+  -c, --config [file]                      JSON configuration file (no file or - means STDIN)
+  -w, --watch                              Continually rebuild
+  -x, --extension <js | coffee | ...>      File extension to assume when resolving module identifiers
+  --relativize                             Rewrite all module identifiers to be relative
+  --follow-requires                        Scan modules for required dependencies
+  --use-provides-module                    Respect @providesModules pragma in files
+  --cache-dir <directory>                  Alternate directory to use for disk cache
+  --no-cache-dir                           Disable the disk cache
+  --source-charset <utf8 | win1252 | ...>  Charset of source (default: utf8)
+  --output-charset <utf8 | win1252 | ...>  Charset of output (default: utf8)
+  -r, --include-runtime                    Prepend the runtime to the output.
+  --disable-async                          Disable transformation of async functions.
+  -h, --help                               output usage information
